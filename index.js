d42bf868014d7a7b987afe762b1942745d30c741
Dulshan Siriwardhana <dulshansiriwardhana70@gmail.com>
2026-03-02 17:35:37 +0530

    updated

---
+++ index.js
@@ -0,0 +1,24 @@
+import cors from 'cors';
+import express from 'express';
+import dotenv from 'dotenv';
+import DBinitialize from './utils/db_init.js';
+import bodyParser from 'body-parser';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+DBinitialize();
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
--- utils/db_init.js
+++ utils/db_init.js
@@ -1,14 +1,17 @@
 import mongoose from "mongoose";
 
-mongoose.connect(process.env.DB_URI, {
-    userNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then(()=>{
-    console.log("Connected to MongoDB");
-})
-.catch((error)=>{
-    console.error("Error connecting to MongoDB:", error);
-});
+const DBinitialize = () => {
+    mongoose.connect(process.env.DB_URI, {
+        userNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(()=>{
+        console.log("Connected to MongoDB");
+    })
+    .catch((error)=>{
+        console.error("Error connecting to MongoDB:", error);
+    });
+}
 
-export default mongoose;
+
+export default DBinitialize;
