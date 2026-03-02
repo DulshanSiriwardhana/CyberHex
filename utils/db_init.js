0ac1492cc23ae040e8438ab842b4e5f68f1237f3
Dulshan Siriwardhana <dulshansiriwardhana70@gmail.com>
2026-03-02 17:26:01 +0530

    db init

--- utils/db_init.js
+++ utils/db_init.js
@@ -1,1 +1,14 @@
 import mongoose from "mongoose";
+
+mongoose.connect(process.env.DB_URI, {
+    userNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(()=>{
+    console.log("Connected to MongoDB");
+})
+.catch((error)=>{
+    console.error("Error connecting to MongoDB:", error);
+});
+
+export default mongoose;
