879189354502a5701909d99ccbc8001bb1443d34
Dulshan Siriwardhana <dulshansiriwardhana70@gmail.com>
2026-03-05 11:51:35 +0530

    updated

--- models/User.js
+++ models/User.js
@@ -5,7 +5,22 @@ const UserSchema = new Schema.Types({
         type: String,
         require: true,
         unique: true
-    }
+    },
+    profilePicture:{
+        type: String,
+        require: false
+    },
+    email:{
+        type: String,
+        require: true,
+        unique: true
+    },
+    password:{
+        type: String,
+        require: true
+    },
+    resetPasswordToken: String,
+    resetPasswordExpires: Date
 });
 
 export const User = mongoose.model("User", UserSchema);
