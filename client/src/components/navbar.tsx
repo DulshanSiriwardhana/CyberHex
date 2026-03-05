3a1dd6e67514354fd42ed366111ea4ccd6eab253
Dulshan Siriwardhana <dulshansiriwardhana70@gmail.com>
2026-03-06 02:55:27 +0530

    updated

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -1,7 +1,7 @@
 const NavBar = () => {
   return (
-    <nav className="bg-gray-800 text-white p-4">
-      <div className="container mx-auto flex justify-between items-center">
+    <nav className="bg-black text-white p-4">
+      <div className="mx-auto flex justify-between items-between">
         <div className="text-lg font-bold">CyberHex</div>
         <div>
           <a href="/" className="px-3 py-2 hover:bg-gray-700 rounded">Home</a>
