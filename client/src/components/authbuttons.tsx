796ce2a66ddc477127366183aa8ee1e2cdc7d25d
Dulshan Siriwardhana <dulshansiriwardhana70@gmail.com>
2026-03-10 17:37:45 +0530

    updated

---
+++ client/src/components/authbuttons.tsx
@@ -0,0 +1,7 @@
+const AuthButtons = () => {
+    return(
+        <button className="text-lg border-4 border-white cursor-pointer flex items-center justify-center px-2 rounded-lg font-extrabold text-white hover:bg-white hover:text-black">Signin</button>
+    )
+}
+
+export default AuthButtons;
