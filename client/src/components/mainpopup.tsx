93ff2c6bc9c4bad157cc7fbab64b4b4d018a7f5f
Dulshan Siriwardhana <dulshansiriwardhana70@gmail.com>
2026-03-11 11:29:19 +0530

    updated

---
+++ client/src/components/mainpopup.tsx
@@ -0,0 +1,9 @@
+const MainPopup = () => {
+    return (
+        <div className="w-80 h-80 flex items-center justify-center bg-black text-white">
+            This is the main popup
+        </div>
+    );
+}
+
+export default MainPopup;
