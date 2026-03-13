fa90d0983f0268b80dcbfedb2c1a24fc87700ee5
Dulshan Siriwardhana <dulshansiriwardhana70@gmail.com>
2026-03-13 20:09:08 +0530

    updated

--- client/src/components/signup/signup.tsx
+++ client/src/components/signup/signup.tsx
@@ -1,34 +1,44 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import SignIn from "../signin";
 import { PopupContext } from "../../contexts/popups";
 import ProgressBar from "./progressbar";
+import UserName from "./username";
+import Email from "./email";
+import Password from "./password";
+import OTP from "./otp";
 
 const SignUp =()=>{
     const { openPopup } = useContext(PopupContext);
+    const [ fullname, setFullName ] = useState('');
+    const [ username, setUsername ] = useState('');
+    const [ email, setEmail ] = useState('');
+    const [ password, setPassword ] = useState('');
+    const [ otp, setOtp ] = useState('');
+    const [ step, _setStep ] = useState(1);
 
     return (
         <div className="w-full max-w-md h-fit flex items-center justify-center flex-col bg-black min-w-[320px] text-white">
             <div className="pt-4">
-                <ProgressBar step={3}/>
+                <ProgressBar step={step}/>
             </div>
             <div className="flex items-center justify-center flex-col gap-4 w-full px-4 pb-4">
-                <div className="flex items-start justify-center flex-col gap-1 w-full">
-                    <div>Email</div>
-                    <input placeholder="cyberhex@wazard.com" className="w-full p-1 rounded-md text-black bg-white px-2 flex items-center justify-center" />
-                </div>
-                <div className="flex items-start justify-center flex-col gap-1 w-full">
-                    <div>Username</div>
-                    <input placeholder="cyber hex" className="w-full p-1 rounded-md text-black bg-white px-2 flex items-center justify-center" />
-                </div>
-                <div className="flex items-start justify-center flex-col gap-1 w-full">
-                    <div>OTP</div>
-                    <input placeholder="OTP" className="w-full p-1 rounded-md text-black bg-white px-2 flex items-center justify-center" />
-                </div>
-                <div className="flex items-start justify-center flex-col gap-1 w-full">
-                    <div>Password</div>
-                    <input placeholder="••••••••" className="w-full p-1 rounded-md text-black bg-white px-2 flex items-center justify-center" />
-                </div>
-                <button className="w-full p-2 rounded-md bg-red-700 hover:bg-red-600 cursor-pointer">Sign Up</button>
+                {
+                    (()=>{
+                        if(step === 1) {
+                            return (<UserName fullname={fullname} username={username} setFullname={setFullName} setUsername={setUsername}/>);
+                        } else if(step === 2) {
+                            return (<Email email={email} setEmail={setEmail}/>);
+                        } else if(step === 3) {
+                            return (<Password password={password} setPassword={setPassword}/>);
+                        } else if(step === 4) {
+                            return (<OTP otp={otp} setOtp={setOtp}/>);
+                        }
+                        else {
+                            return (<div className="text-sm pt-4">Account Created Successfully!</div>);
+                        }
+                    })()
+                }
+                <button onClick={() => _setStep(step + 1)} className="w-full p-2 rounded-md bg-red-700 hover:bg-red-600 cursor-pointer mt-4">{step === 4 ? 'Finish' : 'Next'}</button>
                 <div>Already have an account? <button onClick={()=>openPopup(<SignIn />)} className="text-red-700 hover:underline cursor-pointer">Sign In</button></div>
             </div>
         </div>
