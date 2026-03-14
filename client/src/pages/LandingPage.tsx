30a21898ae26af3fa172d4007277585680c3010c
Dulshan Siriwardhana <dulshansiriwardhana70@gmail.com>
2026-03-14 09:55:05 +0530

    updated

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -1,4 +1,17 @@
+import { useContext, useEffect } from "react";
+import { PopupContext } from "../contexts/popups";
+import Banner from "../components/opening/banner";
+
 const LandingPage = () => {
+    const { openPopup, closePopup } = useContext(PopupContext);
+
+    useEffect(() => {
+        openPopup(<Banner />);
+        return () => {
+            closePopup();
+        }
+    }, []);
+
     return (
         <div className="border w-full h-full flex items-center justify-center">
             This is the landing page. Welcome to CyberHex!
