a886ff60aa29198ee9b0d43768079d46383efb1e
Dulshan Siriwardhana <dulshansiriwardhana70@gmail.com>
2026-03-17 00:41:54 +0530

    const created

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -4,13 +4,9 @@ import { useNavigate } from "react-router-dom";
 import AuthButtons from "./authbuttons";
 import { PopupContext } from "../contexts/popups";
 import MainPopup from "./mainpopup";
+import { items } from "../const/data";
 
-const items = [
-  { name: "Home", path: "/" },
-  { name: "About", path: "/about" },
-  { name: "Services", path: "/services" },
-  { name: "Contact", path: "/contact" },
-];
+
 const NavBar = () => {
     const navigate = useNavigate();
     const [selectedItem, setSelectedItem] = useState(items[0].name);
@@ -35,7 +31,7 @@ const NavBar = () => {
     return (
     <nav className="bg-red-700 text-white p-4 rounded-xl border-2 border-black max-w-boundary mx-auto">
         <div className="mx-auto flex justify-between items-between">
-        <div onClick={handleLogoClick} className="cursor-pointer text-2xl font-extrabold text-white text-center flex items-center justify-center">CyberHex</div>
+        <div onClick={handleLogoClick} className="cursor-pointer text-sm md:text-base lg:text-lg xl:text-2xl font-extrabold text-white text-center flex items-center justify-center">CyberHex</div>
         <div className="flex space-x-2">
             {items.map((item) => (
                 <NavItem
