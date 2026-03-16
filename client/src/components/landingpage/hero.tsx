39b66f1ad786d1aeffa4df8848fe2a62377ea2ff
Dulshan Siriwardhana <dulshansiriwardhana70@gmail.com>
2026-03-16 09:54:35 +0530

    banner assests included

--- client/src/components/landingpage/hero.tsx
+++ client/src/components/landingpage/hero.tsx
@@ -1,8 +1,14 @@
+import banner from '../../assets/videos/banner.mp4';
+
 const Hero=()=>{
     return(
-        <div>
-            <h1 className="text-4xl font-extrabold">Welcome to CyberHex</h1>
-            <p className="text-center text-gray-400 mt-4">Join us in the world of cybersecurity, where you can learn, practice, and compete in a safe and legal environment. Whether you're a beginner or an experienced hacker, CyberHex has something for everyone. Sign up now and start your journey to becoming a cybersecurity expert!</p>
+        <div className="">
+            <div>
+                <video autoPlay loop muted>
+                    <source src={banner} type="video/mp4" />
+                    Your browser does not support the video tag.
+                </video>
+            </div>
         </div>
     )
 }
