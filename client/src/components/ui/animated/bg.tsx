a5591f10a1cba20b9b5d0fdaecf2291867d8ff99
Dulshan Siriwardhana <dulshansiriwardhana70@gmail.com>
2026-03-26 16:29:00 +0530

    updated

--- client/src/components/ui/animated/bg.tsx
+++ client/src/components/ui/animated/bg.tsx
@@ -5,11 +5,11 @@ const Beam=()=>{
     const moveR = Math.floor(Math.random() * 256);
     const moveG = Math.floor(Math.random() * 256);
     const moveB = Math.floor(Math.random() * 256);
-    const speed = Math.random() * 2 + 1;
+    const speed = (Math.random() + 0.001)%1;
 
     return(
-        <div className="bg-black w-full max-h-full flex">
-            <div className="w-[10vw] h-0.5 max-w-20 relative rounded-4xl flex items-center justify-center" style={{ backgroundColor: `rgb(${randR}, ${randG}, ${randB})`, boxShadow: `0 0 10px rgba(${moveR}, ${moveG}, ${moveB}, 1)` }}>
+        <div className="bg-black w-full flex">
+            <div className="w-[10vw] h-0.5 max-w-20 relative rounded-4xl flex items-center justify-center" style={{ transform: `translateX(${speed * 100}vw)`, transitionDuration: `${10000/speed}ms`, backgroundColor: `rgb(${randR}, ${randG}, ${randB})`, boxShadow: `0 0 10px rgba(${moveR}, ${moveG}, ${moveB}, 1)` }}>
                 <div className="absolute w-[1vw] h-[1vw] rounded-full" style={{ backgroundColor: `rgb(${moveR}, ${moveG}, ${moveB})` }}>
                 </div>
             </div>
@@ -19,7 +19,7 @@ const Beam=()=>{
 
 const BG=()=>{
     return(
-        <div className="w-full h-full border overflow-hidden flex">
+        <div className="w-full min-h-full overflow-hidden">
             <Beam />
         </div>
     )
