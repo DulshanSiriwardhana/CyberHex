1d7eb7e00a7fe2eb246aec2391632e9aaf4e3a40
Dulshan Siriwardhana <dulshansiriwardhana70@gmail.com>
2026-03-31 17:22:50 +0530

    mean+square mean

--- ML/models/cpp-modules/main.cpp
+++ ML/models/cpp-modules/main.cpp
@@ -6,6 +6,7 @@ using namespace std;
 int main() {
     double array[3] = {1,3,3};
     double mean = calc_mean(array, 3);
-    cout << mean << endl;
+    double square_mean = calc_square_mean(array, 3);
+    cout << square_mean << endl;
     return 0;
 }
