f3db94ff5d4213ff6eae05a9907b89c4fa768954
Dulshan Siriwardhana <dulshansiriwardhana70@gmail.com>
2026-03-31 17:22:50 +0530

    mean+square mean

--- ML/models/cpp-modules/include/higher_maths.h
+++ ML/models/cpp-modules/include/higher_maths.h
@@ -2,5 +2,6 @@
 #define HIGHER_MATHS_H
 
 double calc_mean(double* array, int size);
+double calc_square_mean(double* array, int size);
 
 #endif
--- ML/models/cpp-modules/src/higher_maths.cpp
+++ ML/models/cpp-modules/src/higher_maths.cpp
@@ -8,3 +8,12 @@ double calc_mean(double* array, int size){
 
     return sum/size;
 }
+
+double calc_square_mean(double* array, int size){
+    double sum = 0.0;
+    for(int i =0;i<size; i++){
+        sum+=array[i]*array[i];
+    }
+
+    return sum/size;
+}
