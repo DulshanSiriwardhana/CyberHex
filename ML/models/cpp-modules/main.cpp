d46c0ef317bada97f57a4acd7f99501ffd463c2c
Dulshan Siriwardhana <dulshansiriwardhana70@gmail.com>
2026-03-31 17:27:55 +0530

    higher order mean

--- ML/models/cpp-modules/main.cpp
+++ ML/models/cpp-modules/main.cpp
@@ -7,6 +7,7 @@ int main() {
     double array[3] = {1,3,3};
     double mean = calc_mean(array, 3);
     double square_mean = calc_square_mean(array, 3);
-    cout << square_mean << endl;
+    double square_mean_2 = calc_n_order_mean(array, 3, 2);
+    cout << square_mean_2 << endl;
     return 0;
 }
