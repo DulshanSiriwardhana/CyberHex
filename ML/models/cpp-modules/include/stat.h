a174ae3f018516289c7f91eba2d13f47e58189f8
Dulshan Siriwardhana <dulshansiriwardhana70@gmail.com>
2026-04-02 01:06:48 +0530

    k-degree polynomial regression

--- ML/models/cpp-modules/include/stat.h
+++ ML/models/cpp-modules/include/stat.h
@@ -5,5 +5,6 @@ double calc_mean(double* array, int size);
 double calc_multiplicative_mean(double* x, double* y, int size);
 double calc_square_mean(double* array, int size);
 double calc_n_order_mean(double* array, int size, int order);
+double calc_n_order_multiplicative_mean(double* x, int x_order, double* y, int y_order, int size);
 
 #endif
--- ML/models/cpp-modules/src/machine_learning_algorithms.cpp
+++ ML/models/cpp-modules/src/machine_learning_algorithms.cpp
@@ -12,3 +12,18 @@ void linear_regression_data(double* dataX, double* dataY, int size, double* retu
     return_data[0] = alpha;
     return_data[1] = beta;
 }
+
+void k_degree_polynomial_regression_data(double* dataX, double* dataY, int size, int degree, double* return_data){
+    double* x_power_means = new double[2*degree + 1];
+    double* xy_means = new double[degree+1];
+    x_power_means[0] = 1;
+    xy_means[0] = calc_mean(dataY, size);
+
+    for(int i =0; i<2*degree;i++){
+        x_power_means[i+1] = calc_n_order_mean(dataX, size, i+1);
+    }
+    
+    for(int i=0;i<degree;i++){
+        xy_means[i+1] = calc_n_order_multiplicative_mean(dataX, i+1, dataY, 1, size);
+    }
+}
