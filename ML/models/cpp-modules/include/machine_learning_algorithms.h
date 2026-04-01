ed7a93783d0d263a3cc83d68d146ff97482c662d
Dulshan Siriwardhana <dulshansiriwardhana70@gmail.com>
2026-04-02 00:10:28 +0530

    multiplicative mean

---
+++ ML/models/cpp-modules/include/machine_learning_algorithms.h
@@ -0,0 +1,6 @@
+#ifndef MACHINE_LEARNING_ALGORITHMS_H
+#define MACHINE_LEARNING_ALGORITHMS_H
+
+double* linear_regression_data(double* dataX, double* dataY, int size);
+
+#endif
---
+++ ML/models/cpp-modules/src/machine_learning_algorithms.cpp
@@ -0,0 +1,5 @@
+#include "machine_learning_algorithms.h"
+
+double* linear_regression_data(double* dataX, double* dataY, int size){
+    
+}
