baf10f06102c83802e2bc9cfdb62dbbf41606fff
Dulshan Siriwardhana <dulshansiriwardhana70@gmail.com>
2026-04-02 10:28:02 +0530

    updated

--- ML/ui/visualizations/src/types/charts.ts
+++ ML/ui/visualizations/src/types/charts.ts
@@ -1,3 +1,15 @@
 export type LineChart = {
+    domain: {
+        start: number,
+        end: number
+    };
+    range: {
+        start: number,
+        end: number
+    };
+    data: {
+        x: number[],
+        y: number[]
+    };
     
 }
