1c60cd2a3fb1e4d01dc811e1df2c48c85fb7d1f8
Dulshan Siriwardhana <dulshansiriwardhana70@gmail.com>
2026-04-02 18:12:55 +0530

    updates

---
+++ ML/ui/visualizations/src/const/Charts.ts
@@ -0,0 +1,16 @@
+import type { LineChartType } from "../types/charts";
+
+export const linechartdata:LineChartType = {
+    data: {
+        x : [-1,-2,-3, 0, 1, 2, 3],
+        y : [-1,-2,-3, 0, 1, 2, 3]
+    },
+    domain: {
+        start: -4,
+        end: 4
+    },
+    range: {
+        start: -4,
+        end: -4
+    }
+}
