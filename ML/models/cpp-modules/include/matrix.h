216b02baac38dd6341d9836859a66459c8e02008
Dulshan Siriwardhana <dulshansiriwardhana70@gmail.com>
2026-04-02 18:51:45 +0530

    init matrix

---
+++ ML/models/cpp-modules/include/matrix.h
@@ -0,0 +1,6 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
+void solve_AX_eq_B(double* A, double* X, double* B, int size);
+
+#endif
---
+++ ML/models/cpp-modules/src/matrix.cpp
@@ -0,0 +1,5 @@
+#include "matrix.h"
+
+void solve_AX_eq_B(double* A, double* X, double* B, int size){
+    
+}
