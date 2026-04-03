9c350d3714d91cafb0abb006da963a8dc77ef9b4
Dulshan Siriwardhana <dulshansiriwardhana70@gmail.com>
2026-04-03 13:49:59 +0530

    matrix solution

--- ML/models/cpp-modules/include/matrix.h
+++ ML/models/cpp-modules/include/matrix.h
@@ -4,6 +4,7 @@
 void removeRowColumn(double* double A, int row, int column, int size, double* double* ret);
 double det2x2(double* double A);
 double det(double* double* A, int size);
+void replaceRow(double* double A, int row, int size, double*  relacingRow, double* double* ret);
 void solve_AX_eq_B(double* double* A, double* X, double* B, int size);
 
 #endif
