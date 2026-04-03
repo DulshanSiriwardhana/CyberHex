2802eaf1dddbcf36cbd08db4fdd98416e81dd35a
Dulshan Siriwardhana <dulshansiriwardhana70@gmail.com>
2026-04-03 14:34:47 +0530

    updated

--- ML/models/cpp-modules/main.cpp
+++ ML/models/cpp-modules/main.cpp
@@ -3,6 +3,7 @@
 #include "basic_maths.h"
 #include "stat.h"
 #include "machine_learning_algorithms.h"
+#include "matrix.h"
 
 using namespace std;
 
@@ -10,7 +11,20 @@ int main() {
     double arrayX[3] = {1,2,4};
     double arrayY[3] = {1,2,3};
     double* n = new double[2];
-    linear_regression_data(arrayX, arrayY, 3, n);
-    cout << n[0] << " " << n[1] << endl;
+
+    double** A = new double*[2];
+    A[0] = new double[2];
+    A[1] = new double[2];
+
+    A[0][0] = 1;
+    A[0][1] = 2;
+    A[1][0] = 1;
+    A[1][1] = 1;
+
+    double X[2];
+    double B[2] = {3,2};
+    solve_AX_eq_B(A, X, B, 2);
+    //linear_regression_data(arrayX, arrayY, 3, n);
+    cout << X[0] << " " << X[1] << endl;
     return 0;
 }
