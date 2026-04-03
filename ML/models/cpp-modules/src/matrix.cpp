06388d01922ae630d9ae4d2eb8f53c507f014c3b
Dulshan Siriwardhana <dulshansiriwardhana70@gmail.com>
2026-04-03 12:54:06 +0530

    matrix solver init

--- ML/models/cpp-modules/src/matrix.cpp
+++ ML/models/cpp-modules/src/matrix.cpp
@@ -1,5 +1,9 @@
 #include "matrix.h"
 
-void solve_AX_eq_B(double* A, double* X, double* B, int size){
-    
+void solve_AX_eq_B(double* double* A, double* X, double* B, int size){
+    for(int i=0;i<size-1;i++){
+        for(int j=i+1;j<size;j++){
+            
+        }
+    }
 }
