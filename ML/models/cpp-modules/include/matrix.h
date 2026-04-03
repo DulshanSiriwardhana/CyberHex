d118158c174843714e643455bfb366fc8ae1d6c0
Dulshan Siriwardhana <dulshansiriwardhana70@gmail.com>
2026-04-03 14:51:53 +0530

    solving matrix issues fixed

--- ML/models/cpp-modules/include/matrix.h
+++ ML/models/cpp-modules/include/matrix.h
@@ -5,6 +5,7 @@ void removeRowColumn(double** &A, int row, int column, int size, double** &ret);
 double det2x2(double** &A);
 double det(double** &A, int size);
 void replaceRow(double** &A, int row, int size, double*  replacingRow, double** &ret);
+void replaceColumn(double** &A, int column, int size, double*  replacingColumn, double** &ret);
 void solve_AX_eq_B(double** &A, double* X, double* B, int size);
 
 #endif
