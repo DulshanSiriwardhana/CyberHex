6ae1bf4ad422e0b849c138fe86e34962e83ea48c
Dulshan Siriwardhana <dulshansiriwardhana70@gmail.com>
2026-04-03 13:19:28 +0530

    det init

--- ML/models/cpp-modules/include/matrix.h
+++ ML/models/cpp-modules/include/matrix.h
@@ -1,6 +1,9 @@
 #ifndef MATRIX_H
 #define MATRIX_H
 
+void removeRowColumn(double* double A, int row, int column, int size, double* double* ret);
+double det2x2(double* double A);
+double det(double* double* A, int size);
 void solve_AX_eq_B(double* double* A, double* X, double* B, int size);
 
 #endif
--- ML/models/cpp-modules/src/matrix.cpp
+++ ML/models/cpp-modules/src/matrix.cpp
@@ -1,9 +1,32 @@
 #include "matrix.h"
 
+void removeRowColumn(double* double A, int row, int column, int size, double* double* ret){
+    
+}
+
+double det2x2(double* double A){
+    return A[0][0]*A[1][1] - A[0][1]*A[1][0];
+}
+
+double det(double* double* A, int size){
+    if(size==2){
+        return det2x2(double* double* A);
+    }
+
+    int k=1;
+    double sum =0;
+    for(int i =0;i<size;i++){
+        sum += k*A[0][i]* det();
+        k *= -1;
+    }
+
+    return sum;
+}
+
 void solve_AX_eq_B(double* double* A, double* X, double* B, int size){
     for(int i=0;i<size-1;i++){
         for(int j=i+1;j<size;j++){
-            
+
         }
     }
 }
