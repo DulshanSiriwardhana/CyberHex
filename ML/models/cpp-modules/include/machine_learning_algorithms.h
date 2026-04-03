ab87ab144c4ca8e117217c8b3201b1dd7e2d7262
Dulshan Siriwardhana <dulshansiriwardhana70@gmail.com>
2026-04-04 00:31:28 +0530

    init

--- ML/models/cpp-modules/include/machine_learning_algorithms.h
+++ ML/models/cpp-modules/include/machine_learning_algorithms.h
@@ -4,5 +4,6 @@
 void linear_regression_data(double* dataX, double* dataY, int size, double* return_data);
 void k_degree_polynomial_regression_data(double* dataX, double* dataY, int size, int degree, double* return_data);
 void logistic(double x);
+void fittest_polynomial_regression_data();
 
 #endif
