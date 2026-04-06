d52fe84b524ee41f6b0bea53b027e54b4f848cdd
Dulshan Siriwardhana <dulshansiriwardhana70@gmail.com>
2026-04-06 07:32:07 +0530

    fix print array

--- ML/models/cpp-modules/include/io.h
+++ ML/models/cpp-modules/include/io.h
@@ -1,7 +1,22 @@
 #ifndef IO_H
 #define IO_H
+#include "iostream"
+
+using namespace std;
 
 template <typename T>
-void print_array(T* array, int size);
+void print_array(T* array, int size){
+    for(int i=0;i<size;i++){
+        if(i==0){
+            cout << "{" << array[i] << ", ";
+        }
+        else if(i==size-1){
+            cout << array[i] << "}" << endl;
+        }
+        else{
+            cout << array[i] << ", ";
+        }  
+    }
+}
 
 #endif
