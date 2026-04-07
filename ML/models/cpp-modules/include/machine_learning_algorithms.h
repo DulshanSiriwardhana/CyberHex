9690cf70239fdc4cef5dea368c1ac729b217e76d
Dulshan Siriwardhana <dulshansiriwardhana70@gmail.com>
2026-04-07 20:51:21 +0530

    knn now working

--- ML/models/cpp-modules/include/machine_learning_algorithms.h
+++ ML/models/cpp-modules/include/machine_learning_algorithms.h
@@ -1,10 +1,11 @@
 #ifndef MACHINE_LEARNING_ALGORITHMS_H
 #define MACHINE_LEARNING_ALGORITHMS_H
+#include "types.h"
 
 void linear_regression_data(double* dataX, double* dataY, int size, double* return_data);
 void k_degree_polynomial_regression_data(double* dataX, double* dataY, int size, int degree, double* return_data);
 void logistic(double x);
 void fittest_polynomial_regression_data();
-int knn(double** &data,  int size, int dimension, int k, double* point, int distance_function);
+int knn(labeledDataPoint* data,  int size, int dimension, int k, double* point, int distance_function);
 
 #endif
