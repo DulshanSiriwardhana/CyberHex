369b32f386a53dfd6b258ddef1de02a574508efa
Dulshan Siriwardhana <dulshansiriwardhana70@gmail.com>
2026-04-09 22:24:21 +0530

    safe update

--- client/src/components/landingpage/hero.tsx
+++ client/src/components/landingpage/hero.tsx
@@ -4,7 +4,7 @@ const Hero=()=>{
     return(
         <div className="w-full flex flex-row items-center justify-between gap-4">
             <div className='w-full border'>
-                hello world
+                Dear Guest
             </div>
             <div>
                 <img src={banner} alt="banner" className="h-auto w-full border-2 border-red-600 rounded-2xl"/>
