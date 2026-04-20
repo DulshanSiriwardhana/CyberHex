8e4d9c94be84261fcb2b082da54939201c75b0e1
Dulshan Siriwardhana <dulshansiriwardhana70@gmail.com>
2026-04-20 21:10:46 +0530

    updated

--- ML/models/cpp-modules/include/activation_functions.h
+++ ML/models/cpp-modules/include/activation_functions.h
@@ -1,5 +1,9 @@
 #ifndef ACTIVATION_FUNCTIONS_H
 #define ACTIVATION_FUNCTIONS_H
 
+void full_linear(double* value);
+void natural_linear(double* value);
+void negative_linear(double* value);
+void full_linear(double* value);
 
 #endif
