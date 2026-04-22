d76b1360321261934f8ac7b758cff31c9472bf7d
Dulshan Siriwardhana <dulshansiriwardhana70@gmail.com>
2026-04-22 16:54:19 +0530

    line math is not completed this is a safe push

--- ML/ui/visualizations/src/components/charts/DataPoint.tsx
+++ ML/ui/visualizations/src/components/charts/DataPoint.tsx
@@ -1,12 +1,17 @@
 import type { LineChartType } from "../../types/charts";
-import { calcPosition } from "../../utils/functions";
+import { calcPosition, getLine } from "../../utils/functions";
 
-const DataPoint=({domain, range, point}:{domain: LineChartType["domain"], range: LineChartType["range"],point: {x: number, y:number}})=>{
+const DataPoint=({domain, range, point, next}:{domain: LineChartType["domain"], range: LineChartType["range"], point: {x: number, y:number}, next?: {x: number, y:number}})=>{
     return(
         <div className="absolute" style={{left:`${calcPosition(domain, range, point)[0]}%`, top: `${calcPosition(domain, range, point)[1]}%`}}>
             <div className="h-2 w-2 rounded-full bg-black flex items-start justify-start ml-[-4px] mt-[-4px]">
                 
             </div>
+            {
+                next && (
+                    <div className="bg-black h-1" style={{width: `${getLine(point, next)[0]}%`}}></div>
+                )
+            }
         </div>
     )
 }
