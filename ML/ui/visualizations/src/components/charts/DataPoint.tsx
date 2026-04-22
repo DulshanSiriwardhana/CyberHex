f7ed9ac42a5cf3ca88521917533c700ba2ab3a10
Dulshan Siriwardhana <dulshansiriwardhana70@gmail.com>
2026-04-22 20:14:51 +0530

    updated

--- ML/ui/visualizations/src/components/charts/DataPoint.tsx
+++ ML/ui/visualizations/src/components/charts/DataPoint.tsx
@@ -3,15 +3,19 @@ import { calcPosition, getLine } from "../../utils/functions";
 
 const DataPoint=({domain, range, point, next}:{domain: LineChartType["domain"], range: LineChartType["range"], point: {x: number, y:number}, next?: {x: number, y:number}})=>{
     return(
-        <div className="absolute" style={{left:`${calcPosition(domain, range, point)[0]}%`, top: `${calcPosition(domain, range, point)[1]}%`}}>
-            <div className="h-2 w-2 rounded-full bg-black flex items-start justify-start ml-[-4px] mt-[-4px]">
-                
+        <div className="absolute w-full h-full">
+            <div className="absolute" style={{left:`${calcPosition(domain, range, point)[0]}%`, top: `${calcPosition(domain, range, point)[1]}%`}}>
+                <div className="h-2 w-2 rounded-full bg-black flex items-start justify-start ml-[-4px] mt-[-4px]">
+                    
+                </div>
             </div>
-            {
-                next && (
-                    <div className="bg-black h-1" style={{width: `${getLine(point, next)[0]}%`}}></div>
-                )
-            }
+            <div className="absolute w-full h-full">
+                {
+                    next && (
+                        <div className="absolute bg-black h-1" style={{left:`${calcPosition(domain, range, point)[0]}%`, top: `${calcPosition(domain, range, point)[1]}%`, width: `${getLine(point, next, domain, range)[0]}%`, rotate:`${getLine(point, next, domain, range)[1]}deg`}}></div>
+                    )
+                }
+            </div>
         </div>
     )
 }
