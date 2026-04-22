cf0d0f19c9af74b38355b9dfb7c0504d2e2c84d8
Dulshan Siriwardhana <dulshansiriwardhana70@gmail.com>
2026-04-22 17:01:06 +0530

    updated the appearance

--- ML/ui/visualizations/src/App.tsx
+++ ML/ui/visualizations/src/App.tsx
@@ -4,8 +4,10 @@ import { linechartdata } from './const/Charts'
 
 function App() {
   return (
-    <div className="w-full h-full min-h-screen flex items-center justify-between">
-      <LineChart info={linechartdata}/>
+    <div className="w-full h-full min-h-screen flex items-center justify-center bg-black">
+      <div className='w-[95%] h-full py-4 mx-8 bg-white border border-white'>
+         <LineChart info={linechartdata}/>
+      </div>
     </div>
   )
 }
