9b7f3710f91105acf067c806045dd81fa6396fbf
Dulshan Siriwardhana <dulshansiriwardhana70@gmail.com>
2026-04-24 07:06:59 +0530

    observer

--- ML/ui/visualizations/src/components/charts/DataPoint.tsx
+++ ML/ui/visualizations/src/components/charts/DataPoint.tsx
@@ -3,40 +3,27 @@ import type { LineChartType } from "../../types/charts";
 import { calcPosition, getLine } from "../../utils/functions";
 
 const DataPoint=({domain, range, point, next}:{domain: LineChartType["domain"], range: LineChartType["range"], point: {x: number, y:number}, next?: {x: number, y:number}})=>{
-    const current_point_ref = useRef<HTMLDivElement | null>(null);
-    const next_point_ref = useRef<HTMLDivElement | null>(null);
-
-    const [current_point, setCurrentPoint] = useState<{x: number | undefined, y:number | undefined}>();
-    const [next_point, setNextPoint] = useState<{x: number | undefined, y:number | undefined}>();
+    const containerRef = useRef<HTMLDivElement | null>(null);
+    const [x1_precentage, y1_precentage] = calcPosition(domain, range, point);
+    const [x2_precentage, y2_precentage] = next ? calcPosition(domain, range, next) : [0, 0];
+    const [size, setSize] = useState({width: 0, height: 0});
 
     useEffect(() => {
-        const observer = new ResizeObserver(() => {
-            if (current_point_ref.current) {
-                const rect = current_point_ref.current.getBoundingClientRect();
-                setCurrentPoint({ x: rect.left, y: rect.top });
-            }
+        if(!containerRef.current) return;
 
-            if (next_point_ref.current) {
-                const rect = next_point_ref.current.getBoundingClientRect();
-                setNextPoint({ x: rect.left, y: rect.top });
-            }
+        const observer = new ResizeObserver(([entry]) => {
+            const {width, height} = entry.contentRect;
+            setSize({width, height});
         });
 
-        if (current_point_ref.current) observer.observe(current_point_ref.current);
-        if (next_point_ref.current) observer.observe(next_point_ref.current);
-
+        observer.observe(containerRef.current);
         return () => observer.disconnect();
-    }, [point, next]);
-
-    useEffect(()=>{
-        console.log(current_point);
-        console.log(next_point);
-    },[current_point, next_point]);
+    }, []);
 
     return(
         <div className="absolute w-full h-full">
             <div className="absolute" style={{left:`${calcPosition(domain, range, point)[0]}%`, top: `${calcPosition(domain, range, point)[1]}%`}}>
-                <div ref={current_point_ref} className="h-2 w-2 rounded-full bg-black flex items-start justify-start ml-[-4px] mt-[-4px]">
+                <div ref={containerRef} className="h-2 w-2 rounded-full bg-black flex items-start justify-start ml-[-4px] mt-[-4px]">
                     
                 </div>
             </div>
