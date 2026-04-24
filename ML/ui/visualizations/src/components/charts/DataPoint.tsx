6ef166e7e987e73fcbc8efef33cd77769c9be853
Dulshan Siriwardhana <dulshansiriwardhana70@gmail.com>
2026-04-24 08:00:41 +0530

    line creation done

--- ML/ui/visualizations/src/components/charts/DataPoint.tsx
+++ ML/ui/visualizations/src/components/charts/DataPoint.tsx
@@ -7,6 +7,7 @@ const DataPoint=({domain, range, point, next}:{domain: LineChartType["domain"], 
     const [x1_precentage, y1_precentage] = calcPosition(domain, range, point);
     const [x2_precentage, y2_precentage] = next ? calcPosition(domain, range, next) : [0, 0];
     const [size, setSize] = useState({width: 0, height: 0});
+    const [{x1, y1, length, angle}, setLine] = useState(getLine(x1_precentage, y1_precentage, x2_precentage, y2_precentage, size.width, size.height));
 
     useEffect(() => {
         if(!containerRef.current) return;
@@ -20,9 +21,26 @@ const DataPoint=({domain, range, point, next}:{domain: LineChartType["domain"], 
         return () => observer.disconnect();
     }, []);
 
+    useEffect(()=>{
+        setLine(getLine(x1_precentage, y1_precentage, x2_precentage, y2_precentage, size.width, size.height))
+    },[size]);
+
     return(
         <div ref={containerRef} className="absolute w-full h-full">
-            <div className="absolute" style={{left: x1_precentage}}></div>
+            <div className="absolute" style={{left: x1, top: y1, transform:"translate(-50%, -50%)"}}>
+                <div className="h-2 w-2 rounded-full bg-blue-600"></div>
+            </div>
+            {next && (
+                <div
+                className="absolute bg-black h-[2px] origin-left"
+                style={{
+                    left: x1,
+                    top: y1,
+                    width: length,
+                    transform: `translateY(-50%) rotate(${angle}deg)`,
+                }}
+                />
+            )}
         </div>
     )
 }
