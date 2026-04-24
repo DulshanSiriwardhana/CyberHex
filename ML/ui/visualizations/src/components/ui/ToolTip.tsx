f836dd069a40a43eb15545833c143634437d8c04
Dulshan Siriwardhana <dulshansiriwardhana70@gmail.com>
2026-04-24 08:37:58 +0530

    updated

---
+++ ML/ui/visualizations/src/components/ui/ToolTip.tsx
@@ -0,0 +1,11 @@
+const ToolTip=({data}: {data: any})=>{
+
+    console.log(data);
+    return(
+        <div className="absolute w-10 h-10 bg-white z-50">
+            hi
+        </div>
+    )
+}
+
+export default ToolTip;
