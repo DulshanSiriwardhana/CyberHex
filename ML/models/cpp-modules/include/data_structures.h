51a16d3c74bab82c3aaf99840119e5e833262cea
Dulshan Siriwardhana <dulshansiriwardhana70@gmail.com>
2026-04-25 17:54:41 +0530

    linked list init

--- ML/models/cpp-modules/include/data_structures.h
+++ ML/models/cpp-modules/include/data_structures.h
@@ -1,6 +1,25 @@
-#ifndef DATA_STRUCTURES
-#define DATA_STRUCTURES
+#ifndef DATA_STRUCTURES_H
+#define DATA_STRUCTURES_H
 
-class LinkedList();
+class Node {
+    public:
+        double data;
+        Node* next;
+
+        Node(double val) {
+            data = val;
+            next = nullptr;
+        }
+};
+
+class LinkedList {
+    private:
+        Node* head;
+
+    public:
+        LinkedList();
+        void insertAtEnd(double val);
+        void display();
+};
 
 #endif
