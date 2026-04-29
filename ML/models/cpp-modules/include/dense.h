a55c244f1b1bdc9daa94a1703b4e782bd070decc
Dulshan Siriwardhana <dulshansiriwardhana70@gmail.com>
2026-04-29 17:25:35 +0530

    dense.h

---
+++ ML/models/cpp-modules/include/dense.h
@@ -0,0 +1,19 @@
+#ifndef DENSE_H
+#define DENSE_H
+
+#include "layer.h"
+
+class Dense : public Layer {
+    private:
+        Matrix weights;
+        Matrix bias;
+        Matrix input;
+    
+    public:
+        Dense(double in, double out);
+
+        Matrix forward(const Matrix& input) override;
+        Matrix backward(const Matrix& grad, double lr) override;
+}
+
+#endif
