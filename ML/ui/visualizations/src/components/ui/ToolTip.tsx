b1b3aa4197266b3a9f2fd521337ac95ef379bbcd
Dulshan Siriwardhana <dulshansiriwardhana70@gmail.com>
2026-05-09 01:07:12 +0530

    tool-tip updated

--- ML/ui/visualizations/src/components/ui/ToolTip.tsx
+++ ML/ui/visualizations/src/components/ui/ToolTip.tsx
@@ -2,8 +2,8 @@ const ToolTip=({x, y, label}: {x: number, y: number, label: string})=>{
     console.log(label);
     
     return(
-        <div className="absolute font-bold text-sm px-4 min-w-28 max-w-32 min-h-14 bg-gray-600 text-white z-50 w-full flex items-center justify-center rounded-sm shadow-md">
-            {JSON.stringify({x, y})}
+        <div className="absolute font-bold text-sm px-4 min-h-10 bg-black text-white z-50 flex items-center justify-center rounded-sm shadow-md overflow-hidden border-2 border-red-600">
+            ({x},<span></span> {y})
         </div>
     )
 }
